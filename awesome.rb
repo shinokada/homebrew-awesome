e488996e3212b398dff6b2bb9bf7989e19171ea5
Shinichi Okada <okada.shin@gmail.com>
2021-06-09 12:48:35 +0900

    version 0.2.5

--- awesome.rb
+++ awesome.rb
@@ -1,8 +1,8 @@
 class Awesome < Formula
     desc "The `awesome` is a simple shell script package manager. Awesome installs a shell script package from a GitHub repo on your macOS/Linux."
     homepage "https://github.com/shinokada/awesome"
-    url "https://github.com/shinokada/awesome/archive/refs/tags/v0.2.3.tar.gz"
-    sha256 "2ba93a0a385d662faac741ce6069d26bac24c4408ac245523e7eefc2ba191bd2"
+    url "https://github.com/shinokada/awesome/archive/refs/tags/v0.2.5.tar.gz"
+    sha256 "6155da82f1c92f2d3db6fa272968eb0d9ac2db506cbb28a440abc9095f25baa1"
     license "MIT"
   
     depends_on "bash"
